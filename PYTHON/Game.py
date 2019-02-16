09f0003e04bcd8f518eb7d41590836887c44e5c4
EMI322585 <46558345+EMI322585@users.noreply.github.com>
2019-02-16 22:03:01 +0200

    Game.py

---
+++ PYTHON/Game.py
@@ -0,0 +1,19 @@
+print('Sveiks! Šī ir "Slepenā skaitļa spēle"')
+name = input("Lūdzu ievadi savu vārdu:")
+print("Sveiks/a," + name)
+
+from random import randint
+number = randint(1,100)
+print("Tagad iedomājies skaitli no 1 līdz 100 un uzraksti to. Tev ir iespēja minēt 5 reizes. Lai veicas! ")
+
+for x in range(5):
+    guess = int(input("Ievadi skaitli:"))
+    if guess == number:
+        print("Apsveicu! Tu uzminēji! Skaitlis ir " + str(guess))
+        break
+    elif guess > number:
+        print("Skaitlis ir mazāks.")
+    elif guess < number:
+        print("Skaitlis ir lielāks.")
+else:
+    print("Diemžēl Tu neuzminēji ar 5 minējumiem. Spēle beigusies!(")
